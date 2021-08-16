254f0beba1b355309bec7d43119c69b6e60a07f3
martamaira <79336883+martamaira@users.noreply.github.com>
2021-08-15 23:44:52 -0300

    Create putEllipsoid.h

---
+++ Parte 2/putEllipsoid.h
@@ -0,0 +1,18 @@
+#ifndef PUTELLIPSOID_H
+#define PUTELLIPSOID_H
+
+#include "sculptor.h"
+#include "figurageometrica.h"
+
+
+class putEllipsoid : public figuraGeometrica {
+    int xcenter, ycenter,zcenter,rx,ry,rz;
+public:
+    putEllipsoid(int xcenter,int ycenter,int zcenter,int rx,int ry,int rz,float r,float g,float b,float a);
+    ~putEllipsoid(){
+
+    };
+    void draw(Sculptor &s);
+};
+
+#endif // PUTELLIPSOID_H
