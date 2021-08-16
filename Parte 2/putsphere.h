17e8bde3ee93ae25966a314c267bb042bae57b59
martamaira <79336883+martamaira@users.noreply.github.com>
2021-08-15 23:44:18 -0300

    Create putsphere.h

---
+++ Parte 2/putsphere.h
@@ -0,0 +1,17 @@
+#ifndef PUTSPHERE_H
+#define PUTSPHERE_H
+
+#include "sculptor.h"
+#include "figurageometrica.h"
+
+class putSphere : public figuraGeometrica {
+    int xcenter, ycenter, zcenter, radius;
+public:
+    putSphere(int xcenter,int ycenter,int zcenter,int radius,float r, float g, float b, float a);
+    ~putSphere(){
+
+    };
+    void draw(Sculptor &s);
+};
+
+#endif // PUTSPHERE_H
